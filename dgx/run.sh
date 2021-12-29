ba77e939f91aafed6054a8ed16991e09b4c45bdd
Simon <Zhyosgames@gmail.com>
2021-12-29 12:59:47 +0100

    1

---
+++ dgx/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME="snake_federated"
+
+ROOT_DIR=$PWD
+
+username=$(whoami)
+
+docker run -it --rm --runtime nvidia \
+-e CUDA_VISIBLE_DEVICES=3 \
+# ALT 2 (comment if you want to do ALT 1)
+-v $ROOT_DIR:/home/$username \
+$IMAGE_NAME \
+
+bash
+#python3 snake_federated_classic.py
+
